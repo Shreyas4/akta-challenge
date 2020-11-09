358f7acfcc2d30c590a3039d218a5978cc06db9d
Shreyash Paratkar <shreyasparatkar@gmail.com>
2020-11-09 03:19:38 -0600

    Changes to worker.js

--- rest.js
+++ rest.js
@@ -1,5 +1,27 @@
+const mysql = require('mysql');
+
+// MySQL Connection Params
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "developer",
+    password: "developer",
+    database: "jobs"
+});
+
+// Auto-create customers table
+con.connect(function (err) {
+    if (err) throw err;
+    let sql = "DROP TABLE IF EXISTS jobs_submitted";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+    });
+    sql = "CREATE TABLE jobs_submitted (id INT PRIMARY KEY, status VARCHAR(10), result VARCHAR(255))";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+    });
+});
+
 const restify = require('restify');
-const worker = require('./worker')
 
 // REST server
 const server = restify.createServer({
@@ -7,6 +29,7 @@ const server = restify.createServer({
     version: '1.0.0',
     url: 'http://localhost',
 });
+
 server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
@@ -14,18 +37,63 @@ server.listen(4000, function () {
     console.log('%s listening at %s', server.name, server.url);
 });
 
-// API Endpoints
+// job counter
+let jobId = 0;
 
+// job queue
+let jobsQueue = [];
+
+// API Endpoints
 server.post('/jobs', function (req, res) {
     if (!req.body || !req.body.input)
         res.end(400);
     else
-        worker.submitJob(req.body.input, res);
+        submitJob(req.body.input, res);
 });
-
 server.get('/jobs/:id', function (req, res) {
-    if(!req.params.id)
+    if (!req.params.id)
         res.end(400);
     else
-        worker.getJobDetails(req.params.id, res);
+        getJobDetails(req.params.id, res).then();
 });
+
+// Helper Functions
+function submitJob(input, res) {
+    jobId++;
+    jobsQueue.push({
+        'id': jobId,
+        'input': input
+    });
+    res.end(JSON.stringify({
+        'id': jobId
+    }));
+}
+
+async function getJobDetails(id, res) {
+    if (id < 1)
+        res.end(400);
+    else
+        con.query(`SELECT status, result FROM jobs_submitted WHERE id=${id}`, function (error, results) {
+            if (error)
+                res.end(400);
+            else
+                res.end(JSON.stringify(results));
+        });
+}
+
+// Process jobs
+setInterval(function () {
+    if (jobsQueue.length > 0)
+        processJob(jobsQueue.shift()).then();
+}, 10);
+
+const { execFile } = require('child_process');
+async function processJob(job) {
+    con.query(`INSERT INTO jobs_submitted(id, status) VALUES (${job.id}, 'queued')`);
+    let childProcess = execFile('node', ['worker', job.input], ((error, stdout) => {
+        if (childProcess.exitCode === 0)
+            con.query(`UPDATE jobs_submitted SET status="success", result="${stdout}" WHERE id=${job.id}`);
+        else
+            con.query(`UPDATE jobs_submitted SET status="error" WHERE id=${job.id}`);
+    }));
+}
--- worker.js
+++ worker.js
@@ -1,106 +1,26 @@
-const mysql = require('mysql');
 const bcrypt = require('bcrypt');
 
-// MySQL Connection Params
-const con = mysql.createConnection({
-    host: "localhost",
-    user: "developer",
-    password: "developer",
-    database: "jobs"
-});
-
-// Auto-create customers table
-con.connect(function (err) {
-    if (err) throw err;
-    let sql = "DROP TABLE IF EXISTS jobs_submitted";
-    con.query(sql, function (err, result) {
-        if (err) throw err;
-    });
-    sql = "CREATE TABLE jobs_submitted (id INT PRIMARY KEY, status VARCHAR(10), result VARCHAR(255))";
-    con.query(sql, function (err, result) {
-        if (err) throw err;
-    });
-});
-
-// job counter
-let jobId = 0;
-let jobsQueue = [];
-
+process.exit(getErrorCode(process.argv[2]));
 function getErrorCode(input) {
     let errorCode = 0;
-    let caps = false, smalls = false, nums = false;
-    for (let ch of input)
-        if (!isNaN(ch * 1))
-            nums = true;
-        else if (ch === ch.toUpperCase())
-            caps = true;
-        else if (ch === ch.toLowerCase())
-            smalls = true;
-    if (!caps)
-        errorCode += 1;
-    errorCode = errorCode * 2;
-    if (!smalls)
-        errorCode += 1;
-    errorCode = errorCode * 2;
-    if (!nums)
-        errorCode += 1;
-    return errorCode
-}
-
-async function processJob(job) {
-    let input = job.input;
-    let id = job.id;
-    con.query(`INSERT INTO jobs_submitted(id, status) VALUES (${id}, 'queued')`);
-    let errorCode;
     if (input.length < 6)
         errorCode = 8;
-    else
-        errorCode = getErrorCode(input);
-    updateJobsTable(id, input, errorCode).then();
-    console.log(`JOB_ID: ${id} ERROR_CODE: ${errorCode}`)
-    return errorCode;
-}
-
-async function updateJobsTable(id, input, errorCode){
-    if(errorCode===0)
-        bcrypt.hash(input, 10, (err, hash) => {
-            let hashRes = `JOB_ID: ${id} COMPUTED_HASH: ${hash}`;
-            console.log(hashRes);
-            con.query(`UPDATE jobs_submitted SET status="success", result="${hashRes}" WHERE id=${id}`);
-        });
-    else
-        con.query(`UPDATE jobs_submitted SET status="error" WHERE id=${id}`);
-}
-
-function acceptJob(input, res) {
-    jobId++;
-    jobsQueue.push({
-        'id': jobId,
-        'input': input
-    });
-    res.end(JSON.stringify({
-        'id': jobId
-    }));
-}
-
-function getJobDetails(id, res) {
-    if(id<1)
-        res.end(400);
-    else
-        con.query(`SELECT status, result FROM jobs_submitted WHERE id=${id}`, function (error, results) {
-            if (error)
-                res.end(400);
-            else
-                res.end(JSON.stringify(results));
-        });
-}
-
-setInterval(function() {
-    if (jobsQueue.length > 0)
-        processJob(jobsQueue.shift()).then();
-}, 10);
-
-module.exports = {
-    submitJob: acceptJob,
-    getJobDetails: getJobDetails
+    else {
+        let caps = false, smalls = false, nums = false;
+        for (let ch of input)
+            if (!isNaN(ch * 1))
+                nums = true;
+            else if (ch === ch.toUpperCase())
+                caps = true;
+            else if (ch === ch.toLowerCase())
+                smalls = true;
+        errorCode += !caps?1:0;
+        errorCode = errorCode * 2;
+        errorCode += !smalls?1:0;
+        errorCode = errorCode * 2;
+        errorCode += !nums?1:0;
+        if (errorCode === 0)
+            process.stdout.write(bcrypt.hashSync(input, 10));
+    }
+    return errorCode
 }
